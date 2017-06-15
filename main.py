0af86d2639a845cf9c741c44ad5269dd4f1ad614
Hunter275 <hunter@hunterthornsberry.com>
2017-06-15 18:28:10 -0400

    Added ability to auto-populate fields and submit

--- main.py
+++ main.py
@@ -3,18 +3,51 @@
 
 import twitter # https://github.com/bear/python-twitter
 import re
+import mechanize
+import httplib
+import urlparse
+import ssl
 
-# Put your keys here
+# Set your email address
+email = "your@email.com"
+
 api = twitter.Api(consumer_key="KEY",
                   consumer_secret="KEY",
                   access_token_key="KEY",
                   access_token_secret="KEY")
 
-# Search results, limited to 1 (count=1)
+# Get the Twitter API results, count=1
 results = api.GetSearch("humblebundle.com?gift", result_type="recent", count=1)
 
-# Parse the result for the url minus the last 3 character (left overs from the formatting of twitter results)
-parsed = re.search("(?P<url>https?://[^\s]+)", results).group("url")[:-3]
+# Turn this list into a string to parse it
+results = str(results)
 
-#print the URL
-print parsed
+# Grab any URL from the status
+url = re.search("(?P<url>https?://[^\s]+)", results).group("url")[:-3]
+
+# Twitter's API shortens all URLs as t.co domains, this grabs the full domain behind it
+parsed = urlparse.urlparse(url)
+h = httplib.HTTPConnection(parsed.netloc)
+h.request('HEAD', parsed.path)
+response = h.getresponse()
+if response.status/100 == 3 and response.getheader('Location'):
+    url = response.getheader('Location')
+else:
+    url = url
+
+# Try to claim the gift
+print "Trying '" + url + "'"
+
+# Use mechanize to populate the two email fields and submit the form
+# Note: Working on bug fixes in this location
+try:
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+    br.open(url)
+    br.select_form(id="gift-redeem")
+    br["email"] = email
+    br.form.find_control(id="email-confirm").__setattr__("value", email)
+    br.submit()
+except Exception as exp:
+    print type(exp)
+    print exp
