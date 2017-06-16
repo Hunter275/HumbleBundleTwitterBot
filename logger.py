bfb950a684ead6b721227c370d881e424a0a7846
Hunter275 <hunter@hunterthornsberry.com>
2017-06-16 15:04:52 -0400

    Removed logger.py, using the logging module

--- logger.py
+++
@@ -1,9 +0,0 @@
-# Hunter Thornsberry http://www.adventuresintechland.com
-# A simple log writer
-
-import datetime
-
-def log(message):
-    file = open("log.txt","a")
-    file.write(str(datetime.datetime.now()) + " - " + message + "\n")
-    file.close()
