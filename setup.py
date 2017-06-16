0cec3ddbf827c76d6a27f33b28ee29d0f5b1fcc3
Hunter275 <hunter@hunterthornsberry.com>
2017-06-16 15:24:57 -0400

    Standarized project structure

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='HumbleBundleTwitterBot',
+    version='0.1.0',
+    description='A twitter bot that attempts to redeem HumbleBundle keys from Tweets',
+    long_description=readme,
+    author='Hunter Thornsberry',
+    author_email='hunter@hunterthornsberry.com',
+    url='https://github.com/Hunter275/HumbleBundleTwitterBot',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
